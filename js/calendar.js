59152c32b74aea0b01eeef296d1ff3ebdc209395
redundant4u <rafch@naver.com>
2019-12-16 18:47:23 +0900

    Add datepicker part

--- js/calendar.js
+++ js/calendar.js
@@ -6,7 +6,7 @@ let prevCal = function () {
 
 	makeYearMonth();
 	makeCal();
-	makeMunguk();
+	makeGrid();
 }
 
 let nextCal = function () {
@@ -14,7 +14,7 @@ let nextCal = function () {
 	
 	makeYearMonth();
 	makeCal();
-	makeMunguk();
+	makeGrid();
 }
 
 let makeYearMonth = function () {
@@ -36,7 +36,7 @@ let makeCal = function () {
 	let count = 0;
 
 	row = calID.insertRow();
-
+	
 	for( let i = 0; i < firstDay.getDay(); i++) {
 		cell = row.insertCell();
 		count += 1;
@@ -53,21 +53,21 @@ let makeCal = function () {
 		//if( today == new Date('2019-12-13') ) { "<font color=green>" + i };
 	}
 
-	makeMunguk(); // first munguk initailzation
+	makeGrid(); // first grid initailzation
 }
 
-let makeMunguk = function () {
+let makeGrid = function () {
 	let firstDay = new Date( today.getFullYear(), today.getMonth(), 1 );
 	let lastDay = new Date( today.getFullYear(), today.getMonth() + 1, 0 );
 
-	let mungukID = document.getElementById("munguk");
+	let gridID = document.getElementById("grid");
 
 	let row = null;
 	let count = 0;
 
-	while( mungukID.rows.length > 0 ) mungukID.deleteRow( mungukID.rows.length - 2 );
+	while( gridID.rows.length > 0 ) gridID.deleteRow( gridID.rows.length - 2 );
 
-	row = mungukID.insertRow();
+	row = gridID.insertRow();
 	for( let i = 1; i <= lastDay.getDate(); i++) {
 		let weekend = new Date( today.getFullYear(), today.getMonth(), i )
 
@@ -75,7 +75,7 @@ let makeMunguk = function () {
 		cell.innerHTML = i;
 
 		if( weekend.getDay() == 0 ) cell.innerHTML = "<font color=red>" + i;
-		if( weekend.getDay() == 1 ) cell.innerHTML = "<font color=blue>" + i;
+		if( weekend.getDay() == 6 ) cell.innerHTML = "<font color=blue>" + i;
 	}
 }
 
@@ -83,17 +83,35 @@ let toggleCal = function () {
 	document.getElementById("yearmonth").setAttribute("colspan", "5");
 	document.getElementsByClassName("grid")[0].removeAttribute("colspan");
 	document.getElementsByClassName("grid")[1].removeAttribute("colspan");
-	document.getElementById("munguk").setAttribute("style", "display: none");
+	document.getElementById("grid").setAttribute("style", "display: none");
 	document.getElementById("calendar").removeAttribute("style");
 }
 
-let toggleMunguk = function () {
+let toggleGrid = function () {
 	//let lastDay = new Date( today.getFullYear(), today.getMonth() + 1, 0);
 
 	document.getElementById("yearmonth").setAttribute("colspan", "23");
 	document.getElementsByClassName("grid")[0].setAttribute("colspan", "4");
 	document.getElementsByClassName("grid")[1].setAttribute("colspan", "4");
 	document.getElementById("calendar").setAttribute("style", "display: none");
-	document.getElementById("munguk").removeAttribute("style");
+	document.getElementById("grid").removeAttribute("style");
 }
 
+$(function () {
+	$.datepicker.setDefaults({
+			dateFormat: 'yy-mm-dd',
+			changeMonth: true,
+			changeYear: true,
+			nextText: '>>',
+			prevText: '<<',
+			dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+			dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+			monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+			monthNamesMin: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+			minDate: "-1M",
+			maxDate: "+1M",
+		});
+
+	$('#startDate').datepicker();
+	$('#endDate').datepicker();
+});
